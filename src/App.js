355042e368aa76624beeda252316f8074489713c
Clementadm <61502539+Clementadm@users.noreply.github.com>
2022-04-10 18:22:48 +0200

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -4,13 +4,16 @@ import { registerRootComponent } from "expo";
 
 import Identification from "./screen/Identification";
 import Event from "./screen/Event";
+import Map from "./screen/Map";
+import Meteo from "./screen/Meteo";
+import Geolocalisation from "./components/Geolocalisation";
 
 export default function App() {
   return (
     <View style={styles.container}>
       {/* <Text>Open up App.js to start working on your app!</Text> */}
       {/* <Identification /> */}
-      <Event />
+      <Geolocalisation />
       <StatusBar style="auto" />
     </View>
   );
