1ecf45d5cac903a2e51ff7b9d599db86efc0d3ec
Clementadm <61502539+Clementadm@users.noreply.github.com>
2022-04-10 18:29:50 +0200

    push app

---
+++ src/App.js
@@ -0,0 +1,31 @@
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View } from "react-native";
+import { registerRootComponent } from "expo";
+
+import Identification from "./screen/Identification";
+import Event from "./screen/Event";
+import Map from "./screen/Map";
+import Meteo from "./screen/Meteo";
+import Geolocalisation from "./components/Geolocalisation";
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      {/* <Text>Open up App.js to start working on your app!</Text> */}
+      {/* <Identification /> */}
+      <Geolocalisation />
+      <StatusBar style="auto" />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+});
+
+registerRootComponent(App);
